89019d8ffe210f7aa2d388f7b25611296b023ff7
Totto16 <tobiausgais@gmail.com>
2022-08-11 00:33:08 +0200

    added the Question Collections

---
+++ src/interactions/ncis/types.ts
@@ -0,0 +1,50 @@
+export interface QuestionCollection {
+  categories: QuestionCollectionCategoriesObject;
+}
+
+export type QuestionCollectionCategories = "main" | "characters" | "episodes" | "quotes";
+
+export type QuestionCollectionCategoriesObject = {
+  [key in QuestionCollectionCategories]: QuestionCollectionDescription;
+};
+
+export interface QuestionCollectionDescription {
+  link: string;
+  info: SingleQuestionCollection[];
+}
+
+export type DateString = string;
+
+export function parseDateString(input: DateString) {
+  return new Date(input);
+}
+
+export type possibleDigits = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export type IDString = string;
+// `${possibleDigits}${possibleDigits}${possibleDigits}${possibleDigits}${possibleDigits}${possibleDigits}`;
+
+export type QuestionDifficulty =
+  | "Average"
+  | "Easier"
+  | "Tough"
+  | "Very Easy"
+  | "Difficult"
+  | "Very Difficult";
+
+export type QuestionRating = "top 10%" | "top 5%" | "top 20%" | "well" | "not rated";
+
+export type URLString = string;
+
+export interface SingleQuestionCollection {
+  description: string;
+  name: string;
+  id: IDString;
+  link: URLString;
+  difficulty: QuestionDifficulty;
+  author: string;
+  questionsNumber: number;
+  amountOfPlays: number;
+  rating: QuestionRating;
+  parsed: DateString;
+}
